243aad19c96da5e4d0b2a5fd2c4b72f7a6f30679
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-01-14 00:29:16 -0500

    Added analytics

---
+++ analytics.js
@@ -0,0 +1,6 @@
+if (document.location.host === "osv.tallcoleman.me") {
+  let s = document.createElement("script");
+  s.src = "https://eu.umami.is/script.js";
+  s.dataset["websiteId"] = "16461310-6b31-4f6f-8551-c14875acf000"
+  document.head.appendChild(s);
+}
